68a9ad575ecdfb5625b1a997407e0225b42f1a8a
pac-yan <pyan@stuy.edu>
2017-01-11 08:47:56 -0500

    Creating a template for all methods

---
+++ SquishyChess.java
@@ -0,0 +1,8 @@
+/*
+Function: to initiate players, set the boards, and reset 
+Attributes:
+1. Current Player
+Methods:
+1. Initiates two instances of class Player and sets color and name for them
+2. 
+ */
