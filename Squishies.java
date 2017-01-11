64451f8c10856ec7bbd77edd7ab5b860945927e2
judaniel4 <cju@stuy.edu>
2017-01-11 10:00:42 -0500

    Finished the basics of the Squishies superclass for all the different chess pieces

---
+++ Squishies.java
@@ -0,0 +1,13 @@
+/*
+The super class for all the different chess pieces.
+Includes the following instance variables:
+1. public boolean isAlive
+Includes the following methods:
+1. public abstract canMove
+ */
+public abstract class Squishies{
+    public Boolean isAlive;
+    public abstract canMove(int startX, int startY, int destinationX, int desinationY){
+
+    }
+}
