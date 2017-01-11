9b8378fd3569d2bcfe5ebe97287497d329916a75
pac-yan <pyan@stuy.edu>
2017-01-11 09:05:01 -0500

    Outlines for functionality

---
+++ King.java
@@ -0,0 +1,17 @@
+/*
+King  
+Function: to be the squishiest of them all
+Attributes:
+sets life to alive
+pieceType is king
+Limits(moving): in any direction but only one space as long as that does not put the piece in danger
+- (x,y) --> (x+1,y)
+- (x,y) --> (x,y+1)
+- (x,y) --> (x-1,y)
+- (x,y) --> (x, y-1)
+Limits(eating): in any direction, but only one space as long as that does no put the piece in danger
+
+Methods:
+isCheck?: checks if the king is in check
+isCheckmate?: checks if the king is in checkmate and if true, releases end game message and declares who won. Asks if they want to reset
+ */
