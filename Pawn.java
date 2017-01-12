b827e0ef05c2e865f2fda376cc3ccebba7fc92ba
pac-yan <pyan@stuy.edu>
2017-01-12 09:13:27 -0500

    "mewmewmew"

---
+++ Pawn.java
@@ -0,0 +1,9 @@
+/*
+Pawn
+Function: to be the squishy squish
+Attributes:
+life is alive
+pieceType Pawn
+Limits(moving): first time, can move one or twice forward. 
+Methods:
+ */
--- SquishyChess.java
+++ SquishyChess.java
@@ -7,3 +7,39 @@ Methods:
 2. setBoard: sets up board
 3. reset: 
  */
+import Player.java;
+
+public class SquishyChess{
+    public String currentPlayer;
+    public int numSwitch;
+    public static void main(String[] args){
+	public String startMessage{
+	    s += 
+	Player player0 = new Player;
+	Player player1 = new Player;
+	
+	
+    }
+    public void firstMove(Player a, Player b){
+	if (a.color == white){
+	    numSwitch = 0;
+	    a.move();
+	}
+	else{
+	    numSwitch = 1;
+	    b.move();
+	}
+    }
+    public void afterFirst(Player a, Player b){
+	while(a.checkmate == false && b.checkmate == false){
+	    if(numSwitch == 1){
+		a.move();
+		numSwitch --;
+	    }
+	    else{
+		b.move();
+		numSwitch ++;
+	    }
+	}
+    }
+}
