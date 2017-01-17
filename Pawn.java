d4964b5a491365709a723f0ccdfb0b1ca3049449
macuna43278 <macuna@stuy.edu>
2017-01-17 09:05:24 -0500

    whelp

--- Pawn.java
+++ Pawn.java
@@ -44,7 +44,7 @@ public class Pawn extends Squishies
 		//otherwise, the pawn cannot move.
 		retBoo = false;
 	    }
-	return retBoo && !(pieceThere(destinationX, destinationY));
+	return retBoo;
     }
 
 }
